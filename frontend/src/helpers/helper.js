69a2d4aaa666a936bb116e5662ee911d79ea6322
rajeshkalidas <rajeshkumar.kalidas@gmail.com>
2020-03-19 05:45:12 +0100

    Create helper.js

---
+++ frontend/src/helpers/helper.js
@@ -0,0 +1,10 @@
+export const compareValues = (key, order = 'asc') => {
+    return function innerSort(a, b) {
+      if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) return 0;
+      const comparison = a[key].localeCompare(b[key]);
+  
+      return (
+        (order === 'desc') ? (comparison * -1) : comparison
+      );
+    };
+  }
